bafe0a2f0bf9e05e50b3f49628f2bb546b074112
이한승 <itstrap1016@gmail.com>
2025-06-15 22:27:58 +0900

    feat/gh-pages 추가 (배포를 위한)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Index';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
