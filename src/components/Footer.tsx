d1eb7523aac9b4bdf68510aaa5ed9304c99d2ca1
이한승 <itstrap1016@gmail.com>
2025-06-16 00:45:34 +0900

    feat/footer Link 클릭시 스크롤 이동

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 
 function Footer() {
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <footer className="border-t border-t-[rgba(0,0,0,0.1)] h-[120px] flex justify-center items-center">
       <p className="text-[#8B8787] font-medium">
         Copyright ©{' '}
-        <Link to="/JobScape/" className="underline">
+        <Link to="/JobScape/" className="underline" onClick={handleClick}>
           GenAIJobScape.co.kr
         </Link>
       </p>
