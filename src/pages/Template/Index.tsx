146e28f35e542c048fbc1525688faf95081917fe
이한승 <itstrap1016@gmail.com>
2025-06-29 00:19:49 +0900

    feat/템플릿 타이틀 추가

--- src/pages/Template/Index.tsx
+++ src/pages/Template/Index.tsx
@@ -2,9 +2,11 @@ import HypothesisTitle from './components/HypothesisTitle';
 
 function Template() {
   return (
-    <main className="pt-[70px] max-w-[1240px] mx-auto bg-[#F8FAFC]">
-      {/* 컨텐츠 내용 */}
-      <HypothesisTitle />
+    <main className="bg-[#F8FAFC] pt-[70px]">
+      <div className="max-w-[1240px] mx-auto">
+        {/* 컨텐츠 내용 */}
+        <HypothesisTitle />
+      </div>
     </main>
   );
 }
--- src/pages/Template/components/HypothesisTitle.tsx
+++ src/pages/Template/components/HypothesisTitle.tsx
@@ -1,5 +1,31 @@
+import { TitleData } from '../../../data/templateData';
+
+const { number, title, summary } = TitleData;
+
 function HypothesisTitle() {
-  return <></>;
+  return (
+    <section className="pt-[200px]">
+      <p className="bg-gradient-to-br from-[#3B48D3] to-[#B0DCFF] bg-clip-text text-transparent font-extrabold text-[64px] leading-none mb-[14px]">
+        {number}
+      </p>
+      <h2 className="text-[40px] leading-none text-[#1E293B] font-extrabold mb-[46px]">
+        {title}
+      </h2>
+      <div>
+        <h3 className="text-2xl leading-none mb-[34px] font-semibold">
+          {summary.title}
+        </h3>
+        <ul className="pl-5 flex flex-col gap-6">
+          {summary.contents.map((content, index) => (
+            <li key={index} className="flex items-center gap-[18px]">
+              <span className="w-[6px] h-[6px] rounded-[50%] bg-[#64748B]"></span>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default HypothesisTitle;
