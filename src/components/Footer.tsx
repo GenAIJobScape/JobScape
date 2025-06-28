22d3169e50b615335516b02e7108845e1c3d78fa
이한승 <itstrap1016@gmail.com>
2025-06-29 00:27:31 +0900

    style/footer 스타일 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   };
 
   return (
-    <footer className="border-t border-t-[rgba(0,0,0,0.1)] h-[120px] flex justify-center items-center max-[1396px]:h-20 max-sm:h-[46px]">
+    <footer className="border-t border-t-[#94A3B81A] h-[80px] flex justify-center items-center bg-[#1E293B] max-[1396px]:h-20 max-sm:h-[46px]">
       <p className="text-[#8B8787] font-medium max-sm:text-[10px]">
         Copyright ©{' '}
         <Link to="/" className="underline" onClick={handleClick}>
