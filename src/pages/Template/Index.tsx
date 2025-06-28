6652571a59229ac14c6dd149ae248a41724ae2ed
이한승 <itstrap1016@gmail.com>
2025-06-28 23:43:49 +0900

    feat/Template 페이지 components 폴더 추가

--- src/pages/Template/Index.tsx
+++ src/pages/Template/Index.tsx
@@ -1,7 +1,10 @@
+import HypothesisTitle from './components/HypothesisTitle';
+
 function Template() {
   return (
-    <main className="pt-[70px] max-w-[1240px] mx-auto">
+    <main className="pt-[70px] max-w-[1240px] mx-auto bg-[#F8FAFC]">
       {/* 컨텐츠 내용 */}
+      <HypothesisTitle />
     </main>
   );
 }
---
+++ src/pages/Template/components/HypothesisTitle.tsx
@@ -0,0 +1,5 @@
+function HypothesisTitle() {
+  return <></>;
+}
+
+export default HypothesisTitle;
