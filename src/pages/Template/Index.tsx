f23625c0ecc0ec69a3ee98885ac93ab9a022f04b
hubintheroot <hubintheroot@gmail.com>
2025-06-29 12:22:18 +0900

    style/Template 페이지 너비 반응형 추가

--- src/pages/Template/Index.tsx
+++ src/pages/Template/Index.tsx
@@ -3,7 +3,7 @@ import HypothesisTitle from './components/HypothesisTitle';
 function Template() {
   return (
     <main className="bg-[#F8FAFC] pt-[70px]">
-      <div className="max-w-[1240px] mx-auto">
+      <div className="mx-auto xl:max-w-[1240px] md:max-w-[624px] max-w-[315px]">
         {/* 컨텐츠 내용 */}
         <HypothesisTitle />
       </div>
