652c11660dea3302752582806d7d4dbf180b11cf
이한승 <itstrap1016@gmail.com>
2025-06-29 14:47:34 +0900

    style/가설 타이틀 반응형

--- src/pages/Template/Index.tsx
+++ src/pages/Template/Index.tsx
@@ -2,8 +2,8 @@ import HypothesisTitle from './components/HypothesisTitle';
 
 function Template() {
   return (
-    <main className="bg-[#F8FAFC] pt-[70px]">
-      <div className="mx-auto xl:max-w-[1240px] md:max-w-[624px] max-w-[315px]">
+    <main className="bg-[#F8FAFC] pt-[70px] max-sm:pt-[46px]">
+      <div className="mx-auto xl:max-w-[1240px] max-md:max-w-[624px] max-sm:px-[60px]">
         {/* 컨텐츠 내용 */}
         <HypothesisTitle />
       </div>
--- src/pages/Template/components/HypothesisTitle.tsx
+++ src/pages/Template/components/HypothesisTitle.tsx
@@ -4,22 +4,27 @@ const { number, title, summary } = TitleData;
 
 function HypothesisTitle() {
   return (
-    <section className="pt-[200px]">
-      <p className="bg-gradient-to-br from-[#3B48D3] to-[#B0DCFF] bg-clip-text text-transparent font-extrabold text-[64px] leading-none mb-[14px]">
+    <section className="pt-[200px] max-md:pt-[100px] max-sm:pt-[54px]">
+      <p className="bg-gradient-to-br from-[#3B48D3] to-[#B0DCFF] bg-clip-text text-transparent font-extrabold text-[64px] leading-none mb-[14px] max-md:text-[48px] max-sm:text-[30px]">
         {number}
       </p>
-      <h2 className="text-[40px] leading-none text-[#1E293B] font-extrabold mb-[46px]">
+      <h2 className="text-[40px] leading-none text-[#1E293B] font-extrabold mb-[46px] max-md:text-[36px] max-md:mb-[36px] max-sm:text-[24px] max-sm:mb-[20px]">
         {title}
       </h2>
       <div>
-        <h3 className="text-2xl leading-none mb-[34px] font-semibold">
+        <h3 className="text-2xl leading-none mb-[34px] font-semibold max-md:text-xl max-sm:text-lg max-sm:leading-[1.4] max-sm:mb-[18px]">
           {summary.title}
         </h3>
-        <ul className="pl-5 flex flex-col gap-6">
+        <ul className="pl-5 flex flex-col gap-6 max-sm:pl-[12px]">
           {summary.contents.map((content, index) => (
-            <li key={index} className="flex items-center gap-[18px]">
-              <span className="w-[6px] h-[6px] rounded-[50%] bg-[#64748B]"></span>
-              <p>{content}</p>
+            <li
+              key={index}
+              className="flex items-center gap-[18px] max-sm:items-baseline max-sm:gap-[10px]"
+            >
+              <span className="w-[6px] h-[6px] rounded-full bg-[#64748B] flex-shrink-0"></span>
+              <p className="text-[#1E293B] text-lg max-md:text-base max-sm:text-sm max-sm:leading-[1.4]">
+                {content}
+              </p>
             </li>
           ))}
         </ul>
