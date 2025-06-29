bb588c3ab9ef50bf329452e727fd28a6192fe442
MinKyeonghyeon <28599@naver.com>
2025-06-29 22:15:35 +0900

    feat/ chat를 위한 데이터 추가

--- src/data/templateData.ts
+++ src/data/templateData.ts
@@ -11,6 +11,22 @@ export const TitleData = {
       '구직자들의 AI 대체 불안감이 취업 준비 방향성에 미치는 영향',
     ],
   },
+  chartData: {
+    title: 'KDT 예산 급증\n(교육 프로그램 급속 확대)',
+    label: '예산(억원)',
+    labels: [2021, 2022, 2023, 2024, 2025],
+    values: [2224, 3068, 4142, 4732, 4781],
+    color: [
+      '#FFB1C1',
+      '#FFD6E0',
+      '#FFDEB4',
+      '#F9D57E',
+      '#B6E89A',
+      '#A8E4EC',
+      '#AFCBFF',
+      '#C6B7FF',
+    ],
+  },
 };
 
 export const dummyData: IContent = {
