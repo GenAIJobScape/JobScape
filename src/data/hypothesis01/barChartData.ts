298c80e0242ebb255a05cd4c21a579da75d2a489
MinKyeonghyeon <28599@naver.com>
2025-07-16 22:07:02 +0900

    feat/ 가설1 데이터 작성

---
+++ src/data/hypothesis01/barChartData.ts
@@ -0,0 +1,9 @@
+import type { IChartDataType } from '../../types/IChartDataType';
+
+export const barChartData: IChartDataType = {
+  title: 'KDT 예산 급증 (교육 프로그램 급속 확대)',
+  label: '예산 (억원)',
+  labels: ['2021', '2022', '2023', '2024', '2025'],
+  values: [2224, 3068, 4142, 4732, 4781],
+  color: ['#FFB1C1', '#FFB1C1', '#FFB1C1', '#FFB1C1', '#FFB1C1'],
+};
