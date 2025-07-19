59995a8ea9f9587a9240d8497195bb88fdef5702
MinKyeonghyeon <28599@naver.com>
2025-07-19 21:35:44 +0900

    feat/ 비교용 barchart만들 수 있도록 dataset으로 수정

--- src/types/IBarChartData.ts
+++ src/types/IBarChartData.ts
@@ -1,7 +1,14 @@
 export interface IBarChartData {
-  title: string; // 차트 상단 제목(Title)
-  label: string; // 데이터셋 라벨(범례에 표시되는 이름)
+  title: string | string[]; // 차트 상단 제목(Title)
   labels: (string | number)[]; // x축 레이블(예: 연도, 카테고리 등)
-  values: number[]; // y축 값(데이터 값, 막대의 높이 등)
-  color: string[]; // 각 데이터(막대 등)의 색상 배열
+  // 기존 단일 데이터셋
+  label?: string; // 데이터셋 라벨(범례에 표시되는 이름)
+  values?: number[]; // y축 값(데이터 값, 막대의 높이 등)
+  color?: string[]; // 각 데이터(막대 등)의 색상 배열
+  // 비교차트용
+  datasets?: {
+    label: string;
+    data: number[];
+    backgroundColor: string | string[];
+  }[];
 }
