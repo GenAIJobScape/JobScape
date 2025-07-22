1a664d9a51586d0e9c901e2ecbd1adb1a5929301
MinKyeonghyeon <28599@naver.com>
2025-07-22 22:59:12 +0900

    feat/Chart를 가로로 두 개 나열할 수 있는 그리드 컴포넌트 생성

---
+++ src/pages/Hypothesis01/components/ChartGrid.tsx
@@ -0,0 +1,25 @@
+import PieChart from "../../../components/PieChart";
+import BarChart from "../../../components/BarChart";
+import type { IPieChartData } from "../../../types/IPieChartData";
+import type { IBarChartData } from "../../../types/IBarChartData";
+import type { ChartOptions } from "chart.js";
+
+interface ChartGridProps {
+  pieData: IPieChartData;
+  barData: IBarChartData;
+  barOptions: ChartOptions<'bar'>;
+}
+
+const ChartGrid = ({ pieData, barData, barOptions }: ChartGridProps) => (
+  <div className="w-full h-[800px] grid grid-cols-2">
+    <div className="h-[800px] flex items-center justify-center">
+      <PieChart chartData={pieData} />
+    </div>
+    <div className="h-[800px] flex items-center justify-center">
+      <BarChart chartData={barData} options={barOptions} />
+    </div>
+  </div>
+);
+
+export default ChartGrid;
+
