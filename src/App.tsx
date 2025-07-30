4ec0950b58e6ac6fe344a2f100ce161ac7296d66
이한승 <itstrap1016@gmail.com>
2025-07-30 14:13:11 +0900

    feat/ vite 배포 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Index';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import Hypothesis01 from './pages/Main/components/Hypothesis';
+import Hypothesis01 from './pages/Hypothesis01/Index';
 
 function App() {
   return (
