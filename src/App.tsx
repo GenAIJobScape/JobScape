b87d0b741e8575febfe473063954f945c0e9ea89
이한승 <itstrap1016@gmail.com>
2025-07-30 14:02:04 +0900

    feat/배포용 앱으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Index';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import Hypothesis01 from './pages/Main/components/Hypothesis';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/hypothesis/01" element={<Hypothesis01 />} />
       </Routes>
       <Footer />
     </Router>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import DevelopApp from './DevelopApp';
-// import App from './App.tsx'
+// import DevelopApp from './DevelopApp';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <DevelopApp />
+    <App />
+    {/* <DevelopApp /> */}
   </StrictMode>
 );
