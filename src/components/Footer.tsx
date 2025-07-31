9cf7641da9b1dc83334e30c503a8f6143aa5c5ef
이한승 <itstrap1016@gmail.com>
2025-07-31 12:24:45 +0900

    feat/ yml 파일 cname 경로 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,11 @@
 import { Link } from 'react-router-dom';
 
 function Footer() {
-  const handleClick = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
   return (
     <footer className="border-t border-t-[#94A3B81A] h-[80px] flex justify-center items-center bg-[#1E293B] max-sm:h-[46px]">
       <p className="text-[#8B8787] font-medium max-sm:text-[10px]">
         Copyright ©{' '}
-        <Link to="/" className="underline" onClick={handleClick}>
+        <Link to="/" className="underline">
           GenAIJobScape.co.kr
         </Link>
       </p>
